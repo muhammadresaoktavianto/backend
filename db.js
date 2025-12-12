ad76fa0d0d70d1328a820a718d5ad3f9e35b687e
Muhammad Resa Oktavianto <muhammadresaoktavianto@gmail.com>
2025-12-12 10:13:05 +0700

    fix: update db config for railway

--- db.js
+++ db.js
@@ -1,9 +1,20 @@
-require("dotenv").config();
+// Load .env dulu (buat lokal testing)
+require("dotenv").config(); 
 const mysql = require("mysql");
 
+// Debug cek apakah ENV terbaca
+console.log("ENV CHECK:", {
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  pass: process.env.DB_PASS ? "*****" : "",
+  db: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+// Koneksi MySQL
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME
