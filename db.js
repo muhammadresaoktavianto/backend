b19b918c8d9b2a5e1fc0a39f4a51c922cc85760a
Muhammad Resa Oktavianto <muhammadresaoktavianto@gmail.com>
2025-12-12 10:00:58 +0700

    fix: update db config for railway

--- db.js
+++ db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "bankcp"
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
 db.connect(err => {
-  if (err) console.error("❌ Gagal koneksi ke MySQL:", err);
-  else console.log("✅ Koneksi ke MySQL berhasil");
+  if (err) {
+    console.error("❌ Gagal koneksi ke MySQL:", err);
+  } else {
+    console.log("✅ Koneksi ke MySQL berhasil");
+  }
 });
 
 module.exports = db;
